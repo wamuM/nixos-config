68ea16ce5b50bd772b9a6c27db788922ab2c65b0
wamu_M <contact@wamu-m.com>
2026-02-04 15:19:31 +0100

    Make bash not depend on bashrc.d module

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -7,7 +7,10 @@ config = lib.mkIf config.bundle.bash.enable {
    programs.bash = {
    	enable = true;
 	enableCompletion = true;
-	initExtra = "source ~/.bashrc.d.sh";
+	initExtra = ''
+    if [ -f "$HOME/.bashrc.d.sh" ];then
+        source ~/.bashrc.d.sh
+    fi'';
 	historyControl = ["ignoreboth"];
 	historyFileSize = 10000;
 	historySize = 1000;
