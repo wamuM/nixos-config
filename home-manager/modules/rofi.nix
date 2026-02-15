cbe8bcbf197ad8589e6d9f527e4eaf90e38e47c8
wamu_M <contact@wamu-m.com>
2026-02-15 17:48:23 +0100

    Add launcher alias for rofi

--- home-manager/modules/rofi.nix
+++ home-manager/modules/rofi.nix
@@ -4,6 +4,16 @@
 	rofi.enable = lib.mkEnableOption "enables rofi";
   }; 
   config = lib.mkIf config.bundle.rofi.enable {
+    home.file.".local/bin/launcher" = {
+        text = ''
+            #!/usr/bin/env bash
+            rofi -show drun
+        '';
+        executable = true;
+    };
+    home.sessionPath = [
+        "$HOME/.local/bin"
+    ];
     programs.rofi = {
         enable = true;
         theme = "sidebar";
