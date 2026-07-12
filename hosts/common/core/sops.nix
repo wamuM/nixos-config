c7ed5b2348c325d97ace796134ad4175b1aced5e
wamu_M <contact@wamu-m.com>
2026-07-12 17:40:12 +0200

    Add sops package

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -3,6 +3,9 @@
     imports = [
         inputs.sops-nix.nixosModules.sops
     ];
+    environment.systemPackages = with pkgs; [
+        sops
+    ];
 
     sops = {
         defaultSopsFile = ../../../secrets/common.yaml;
