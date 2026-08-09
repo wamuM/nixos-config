cb13d87b0beeeed719d59700237781f7d96f3fb0
wamu_M <contact@wamu-m.com>
2026-08-09 11:00:44 +0200

    Add bitwarden extension to firefox

--- home/wamu-m/common/optional/firefox.nix
+++ home/wamu-m/common/optional/firefox.nix
@@ -43,9 +43,13 @@ config = {
             BlockAboutProfiles = false;
             OverrideFirstRunPage = "";
             NoDefaultBookmarks = true;
-            DefaultDownloadDirectory = "\${home}/Downloads";
+            DefaultDownloadDirectory = "\${HOME}/Downloads";
             ExtensionSettings = {
                 "*".installation_mode = "blocked";
+                "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+                    installation_mode = "force_installed";
+                };
                 "uBlock0@raymondhill.net" = {
                     install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
                     installation_mode = "force_installed";
